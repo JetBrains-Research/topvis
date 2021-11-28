a2ae51cf257a6e435ec446ceb09b75b37cc48e6a
Ruslan Salkaev <rsalkaev@gmail.com>
2021-11-28 22:18:47 +0300

    Update script

--- .space.kts
+++ .space.kts
@@ -3,7 +3,7 @@ job("Generate and publish sites") {
         shellScript {
             interpreter = "/bin/bash"
             content = """
-                ./generate-public-site.sh
+                ./topics.sh input/input.txt
                 cp -r site $mountDir/share
             """.trimIndent()
         }
--- topics.sh
+++ topics.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-apt update
-apt install python3-pip
-apt install git
+requirements=(pip3 python3 git)
+for item in ${requirements[*]}
+do
+    if ! $item --version $>/dev/null; then
+        echo "Please install $item"
+        exit 1
+    fi
+done
 git clone git@github.com:salkaevruslan/sosed.git
 cd sosed/
 git checkout topics
@@ -9,9 +14,9 @@ git pull
 pip3 install cython
 pip3 install -r requirements.txt
 python3 -m sosed.setup_tokenizer
-python3 -m sosed.run_topics -i $1 -o out --force
-cp sosed/out/topics.txt src/main/resources/topics.txt
+python3 -m sosed.run_topics -i ../"$1" -o out --force
 cd ..
+cp sosed/out/topics.json src/main/resources/topics.json
 ./gradlew clean build
 cp build/distributions/index.html site/public/
 cp build/distributions/topvis.js site/public/
