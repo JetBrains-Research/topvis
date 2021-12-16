fdeadb4dfa7fdc68edaecfeda9d3d3b9f32a2684
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2021-12-16 16:06:11 +0300

    generate and publish internal site WIP

--- .space.kts
+++ .space.kts
@@ -1,4 +1,4 @@
-job("Generate and publish sites") {
+job("Generate and publish public site") {
     startOn {
         gitPush {
             branchFilter {
@@ -28,6 +28,43 @@ job("Generate and publish sites") {
                 siteSource,
                 "topic-vis-demo-public",
                 HostingSiteSettings(public = true)
+            )
+        }
+    }
+}
+
+job("Generate and publish internal site") {
+    startOn {
+        gitPush {
+            enabled = false
+        }
+    }
+    
+    container("Generate internal site", "openkbs/jre-mvn-py3") {
+        env["USER"] = Secrets("git-user")
+        env["TOKEN"] = Secrets("git-token")
+        shellScript {
+            interpreter = "/bin/bash"
+            content = """
+                echo "tcd/loader-sample" >> input/input-internal.txt
+                ./topics-internal.sh input/input-internal.txt ${'$'}TOKEN
+                mkdir $mountDir/share/site
+                cp -r site/internal $mountDir/share/site
+            """.trimIndent()
+        }
+    }
+
+    container("Publish internal site", "openkbs/jre-mvn-py3") {
+        kotlinScript { api ->
+            val siteSource = "$mountDir/share/site/internal"
+            val sourceDir = java.io.File(siteSource)
+            if (!sourceDir.isDirectory || sourceDir.list().isEmpty()) {
+                error("Directory $siteSource is empty")
+            }
+            api.space().experimentalApi.hosting.publishSite(
+                siteSource,
+                "topvis",
+                HostingSiteSettings(public = false)
             )
         }
     }
---
+++ topics-internal.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+requirements=(pip3 python3 git)
+for item in ${requirements[*]}
+do
+    if ! $item --version $>/dev/null; then
+        echo "Please install $item"
+        exit 1
+    fi
+done
+mkdir -p temp
+cd temp
+echo -n > ./repos.txt
+while read line; do
+  echo $line
+  uri="https://$USER:$2@git.jetbrains.team/${line}.git"
+  echo "Cloning $(basename "$line" .git)"
+  git clone --depth 1 "$uri"
+  cd ${line##*/} && ls -la && cd ..
+  touch repos.txt
+  echo ../temp/$(basename "${line##*/}") >> ./repos.txt
+  cat repos.txt
+done <../"$1"
+cd .. 
+git submodule init
+git submodule update
+mkdir -p site/internal/topics
+mkdir -p src/main/resources/topics_internal/
+cd sosed/
+mkdir -p out
+cd out
+rm -r *
+cd ..
+pip3 install cython
+pip3 install -r requirements.txt
+python3 -m sosed.setup_tokenizer
+python3 -m sosed.run_topics -i "../temp/repos.txt" -o "out" --force --local
+cd ..
+cp sosed/out/topics.json src/main/resources/topics_internal/sosed.json
+cd tfidf
+pip3 install -U scikit-learn
+python3 -m tfidf -i "../temp/repos.txt" -o "out" --local
+cd ..
+cp tfidf/out/topics.json src/main/resources/topics_internal/tfidf.json
+./gradlew clean build
+cp build/distributions/index.html site/internal/
+cp build/distributions/topvis.js site/internal/
+cp build/distributions/topvis.js.map site/internal/
+cp -r build/distributions/topics_internal/* site/internal/topics
+rm -rf ./temp
