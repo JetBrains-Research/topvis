89426dc3d43f056685d56e4facbc2f2d5bd5a0f6
Ruslan Salkaev <rsalkaev@gmail.com>
2022-01-03 13:38:13 +0300

    Add resources copy to script

--- topics.sh
+++ topics.sh
@@ -42,6 +42,7 @@ ls *.json > ../resources.txt
 cd ../../../..
 ./gradlew clean build
 cp build/distributions/index.html site/public/
+cp build/distributions/resources.txt site/public/
 cp build/distributions/topvis.js site/public/
 cp build/distributions/topvis.js.map site/public/
 cp -r build/distributions/topics site/public/
