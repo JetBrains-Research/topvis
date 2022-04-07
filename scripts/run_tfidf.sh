13408c9f5efc3af764e6a3effecb34db728960ab
Ruslan Salkaev <rsalkaev@gmail.com>
2022-04-07 16:04:28 +0200

    Update scikit-learn version

--- scripts/run_tfidf.sh
+++ scripts/run_tfidf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd tfidf
-pip3 install -U scikit-learn==1.0.1
+pip3 install -U scikit-learn==1.0.2
 python3 -m tfidf -i "$1" -o "out" --local
 cd ..
 cp tfidf/out/topics.json src/main/resources/topics/tfidf.json
