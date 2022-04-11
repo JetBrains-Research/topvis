94a19b775862607dd558ded2230dc92018d80eaf
Ruslan Salkaev <rsalkaev@gmail.com>
2022-04-11 21:33:42 +0300

    Use venv to run models

--- scripts/run_sosed.sh
+++ scripts/run_sosed.sh
@@ -4,9 +4,15 @@ mkdir -p out
 cd out
 rm -r *
 cd ..
+python3 -m pip install --user virtualenv
+python3 -m venv ./venv
+source ./venv/bin/activate
+which python3
 pip3 install cython
 pip3 install -r requirements.txt
 python3 -m sosed.setup_tokenizer
 python3 -m sosed.run_topics -i "$1" -o "out" --force --local
+deactivate
+rm -rf ./venv
 cd ..
 cp sosed/out/topics.json src/main/resources/topics/sosed.json
--- scripts/run_tfidf.sh
+++ scripts/run_tfidf.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd tfidf
+python3 -m pip install --user virtualenv
+python3 -m venv ./venv
+source ./venv/bin/activate
+which python3
 pip3 install -U scikit-learn==1.0.2
 python3 -m tfidf -i "$1" -o "out" --local
+deactivate
+rm -rf ./venv
 cd ..
 cp tfidf/out/topics.json src/main/resources/topics/tfidf.json
